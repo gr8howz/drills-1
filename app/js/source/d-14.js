0c928521b235537c5fec8654fb8c65325992425c
gr8howz <souledout81@yahoo.com>
2014-04-24 13:54:06 -0500

    added new filenames

--- app/js/source/d-14.js
+++ app/js/source/d-14.js
@@ -12,8 +12,14 @@
     var symbol = $('#symbol').val().trim().toUpperCase();
     var url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=' +symbol+'&callback=?';
     $.getJSON(url, function(data){
-    var $div = $('#quote').text('$' + data.LastPrice);
-    $('#quote').append($div);
+      var $div1 = $('#quote').text(data.Symbol);
+      var $div2 = $('#quote').text(data.Name);
+      var $div3 = $('#quote').text('$' + data.LastPrice);
+
+      $('#quote').append($div1);
+      $('#quote').append($div2);
+      $('#quote').append($div3);
+
     });
   }
 
